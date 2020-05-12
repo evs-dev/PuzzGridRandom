d6ac8a5925472a6bac66c896f82774a1314d5b02
Evan <evansmeath@gmail.com>
2020-05-12 15:21:30 +0100

    Cleaned up js

--- puzzgridrandom.js
+++ puzzgridrandom.js
@@ -1,13 +1,12 @@
-
 const baseURL = "https://www.puzzgrid.com/grid/";
 const maxGrids = 30000;
 
 function random() {
     let gridNumber = Math.floor(Math.random() * maxGrids);
     let url = baseURL + gridNumber.toString();
-    open_site_in_new_tab(url);
+    openSite(url);
 }
 
-function open_site_in_new_tab(url) {
+function openSite(url) {
     window.open(url, '_blank')
 }
